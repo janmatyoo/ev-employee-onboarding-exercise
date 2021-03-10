463a8aee2afa17fbc7f9958824d9c8ea06adb276
Matthew Sarmiento <sarmiento.johnmatt@gmail.com>
2021-03-10 11:59:52 +0800

    Added controller

--- backend/api/routes/employees.js
+++ backend/api/routes/employees.js
@@ -1,35 +1,16 @@
 const express = require('express');
 const fs = require('fs');
-const router = express.Router();
+const router = express.Router(); 
+const EmployeeController = require('../controllers/employees');
 
-router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: "List all employees"
-    });
-});
+router.get('/', EmployeeController.get_all_employees);
 
-router.get('/:id', (req, res, next) => {
-    res.status(200).json({
-        message: "List specific employee"
-    });
-});
+router.get('/:id', EmployeeController.get_specific_employee);
 
-router.post('/', (req, res, next) => {
-    res.status(200).json({
-        message: "Added employee"
-    });
-});
+router.post('/', EmployeeController.add_new_employee);
 
-router.put('/:id', (req, res, next) => {
-    res.status(200).json({
-        message: "Updated employee"
-    });
-});
+router.put('/:id', EmployeeController.update_selected_employee);
 
-router.delete('/:id', (req, res, next) => {
-    ees.status(200).json({
-        message: "Deleted employee"
-    });
-});
+router.delete('/:id', EmployeeController.delete_selected_employee);
 
 module.exports = router
